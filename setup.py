82e5759654d676f66be591ed6e30c8a4429ca46b
brucksmeister <andreas.bruckmoser98@gmail.com>
2024-08-30 11:55:03 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='JamBot',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pretty_midi',
+        'mido',
+        'tensorflow',
+        'keras',
+        'matplotlib',
+    ],
+    author='Brucksmeister',
+    author_email='',
+    description='',
+    url='https://github.com/brucksmeister/JamBot.git',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
